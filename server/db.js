8b9bf03c9c58eec538cf4166bdc4fc362b87ec7e
Ganesh keesara <ganeshkeesara@Ganeshs-MacBook-Air.local>
2025-11-25 00:15:22 +0530

    Secure MongoDB URI with .env and update gitignore

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://ganeshkeesara:Ganesh.k123@cluster0.cazvrzx.mongodb.net/lora-water-monitor';
+const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri)
     .then(() => console.log('Connected to MongoDB'))
